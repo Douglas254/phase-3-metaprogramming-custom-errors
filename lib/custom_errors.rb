1aed70daba38d95e932282ed7928a9adf121ddbc
Douglas Obara <douglasobara97@gmail.com>
2022-08-24 07:53:08 +0300

    commit tree 0b2a2b91aad6d1a642aca8b64fbe2d8ec5b45337

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,3 +1,13 @@
+# you must give the get_married method an argument of an instance of the person class!
+# Beyonce
+
+# Person
+# #get_married
+# when passed a Person instance as an argument
+# sets the the partners of each person
+# when passed something other than a Person instance as an argument
+# outputs an error message to the terminal
+
 class Person
   attr_accessor :partner, :name
 
@@ -6,16 +16,25 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
   end
 
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
